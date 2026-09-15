e64df3d4f15ea91a1c453aaef2a2c33292cadaa9
kuyk15820-cpu <kuyk15820@gmail.com>
2026-09-16 00:49:50 +0700

    Update dashboard_tab.dart

--- lib/models/dashboard_tab.dart
+++ lib/models/dashboard_tab.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import '../models/dashboard_stats.dart';
+import '../models/model.dart';
 
 class DashboardTab extends StatelessWidget {
   final Future<DashboardStats> statsFuture;
