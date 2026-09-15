a347500b2c900a63cd9fc943d6947f630c11ab82
kuyk15820-cpu <kuyk15820@gmail.com>
2026-09-15 11:23:48 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'models/dashboard_stats.dart';
-import 'screens/dashboard_tab.dart';
+import 'models/dashboard_tab.dart';
 import 'services/api_service.dart';
 
 void main() {
